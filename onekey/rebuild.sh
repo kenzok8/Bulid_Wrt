a9e7b0d7b0f78e594b516a3ee1eaba4a4b34edc5
kenzok8 <kenzok8@gmail.com>
2021-11-17 00:10:46 +0800

    int

--- onekey/rebuild.sh
+++ onekey/rebuild.sh
@@ -46,8 +46,8 @@ fi
 
 echo
 
-read -p "请输入后台地址 [回车默认10.0.0.1]: " ip
-ip=${ip:-"10.0.0.1"}
+read -p "请输入后台地址 [回车默认192.168.1.252]: " ip
+ip=${ip:-"192.168.1.252"}
 echo "您的后台地址为: $ip"
 
 rm -Rf feeds package/feeds common files diy tmp
@@ -65,11 +65,11 @@ if [ -f "devices/$firmware/diy.sh" ]; then
 fi
 cp -Rf ./diy/* ./
 if [ -f "devices/common/default-settings" ]; then
-	sed -i 's/10.0.0.1/$ip/' devices/common/default-settings
+	sed -i 's/192.168.1.252/$ip/' devices/common/default-settings
 	cp -f devices/common/default-settings package/*/*/my-default-settings/files/uci.defaults
 fi
 if [ -f "devices/$firmware/default-settings" ]; then
-	sed -i 's/10.0.0.1/$ip/' devices/$firmware/default-settings
+	sed -i 's/192.168.1.252/$ip/' devices/$firmware/default-settings
 	cat devices/$firmware/default-settings >> package/*/*/my-default-settings/files/uci.defaults
 fi
 if [ -n "$(ls -A "devices/common/patches" 2>/dev/null)" ]; then
